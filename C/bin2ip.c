c9fb56510ea645013180686c521671e10e46815c
Mane Hambardzumyan <manehambartsumyan1@gmail.com>
2020-01-27 02:32:06 +0400

    bin2ip change to function
    Changed bin2ip, made it a function with pointer that returns array of int[4]. And get as an argument the array of binary numbers.

--- C/bin2ip.c
+++ C/bin2ip.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
-int main() {
-  
-int x[] = {0,1,0,0,1,0,0,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,1,0,1,1,0,1,0,0,1,0};
+int * bintoip(int binary[]) {
 
-int y[] = {128,64,32,16,8,4,2,1};
+   int *ip = (int*) malloc(4*sizeof(int));
+   int num = 0, y[] = {128,64,32,16,8,4,2,1}, i, j;
 
-int num = 0;
-int i,j;
 for (int i = 0; i < 4; i++) {
 	for (int j = 0; j < 8; j++ ) {
-		num = num + y[j] * x[8*i+j];
-			
+		num = num + y[j] * binary[8*i+j];
 	}
-
-	printf("%d.", num);
+	ip[i] = num;
 	num = 0;	
 }
 
+	return ip;
 }
